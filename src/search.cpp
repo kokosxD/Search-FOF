f6f277cbc4797b85fc3b59cce684b71247f4959a
kokos <kokos1811@outlook.com.gr>
2020-02-20 09:25:56 +0200

    Added error handling for permission errors

--- src/search.cpp
+++ src/search.cpp
@@ -3,26 +3,40 @@
 #include <vector>
 #include <locale>
 #include "path.h"
+#include "utils.h"
 #include "kokostring.h"
+#include "search.h"
+
+File::File(const std::string& _dir){
+	dir = _dir;
+	size = kokos::path::GetSize(dir);
+}
 
 namespace kokos{
 	std::vector<std::string> Search(const std::string& dir, std::string& keyword){
 		std::vector<std::string> results;
 		for(std::string result : kokos::path::GetFOF(dir)){
-			if(kokos::path::IsDir(result)){
-				if(kokos::string::Lowercase(std::filesystem::path(result).filename().string()).find(kokos::string::Lowercase(keyword)) != std::string::npos){
-					results.push_back(std::filesystem::path(result).filename().string());
-					// results.push_back(result);
+			try{
+				if(kokos::path::IsDir(result)){
+					if(kokos::string::Lowercase(std::filesystem::path(result).filename().string()).find(kokos::string::Lowercase(keyword)) != std::string::npos){
+						results.push_back(std::filesystem::path(result).filename().string());
+
+						// Add the full path
+						// results.push_back(result);
+					}
+					std::vector<std::string> r = Search(kokos::path::Join(dir, result), keyword);
+					results.insert(results.end(), r.begin(), r.end());
 				}
-				std::vector<std::string> r = Search(kokos::path::Join(dir, result), keyword);
-				results.insert(results.end(), r.begin(), r.end());
-			}
-			else if(kokos::path::IsFile(result)){
-				if(kokos::string::Lowercase(std::filesystem::path(result).filename().string()).find(kokos::string::Lowercase(keyword)) != std::string::npos){
-					results.push_back(std::filesystem::path(result).filename().string());
-					// results.push_back(result);
+				else if(kokos::path::IsFile(result)){
+					if(kokos::string::Lowercase(std::filesystem::path(result).filename().string()).find(kokos::string::Lowercase(keyword)) != std::string::npos){
+						results.push_back(std::filesystem::path(result).filename().string());
+
+						// Add the full path
+						// results.push_back(result);
+					}
 				}
 			}
+			catch(std::filesystem::filesystem_error){}
 		}
 		return results;
 	}
